f018b9ccb327ee7f3889c33bd65c338907be2afb
sanga1563 <sanga1563@naver.com>
2021-04-01 17:53:01 +0900

    [ADD] Bottom navigation view

--- app/src/main/java/com/example/watchacopy/SplashActivity.java
+++ app/src/main/java/com/example/watchacopy/SplashActivity.java
@@ -1,6 +1,33 @@
 package com.example.watchacopy;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private String TAG = SplashActivity.class.getSimpleName();
+    private Handler handler;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new splashHandler(), 500);
+
+    }
+
+    private class splashHandler implements Runnable {
+
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
 }
